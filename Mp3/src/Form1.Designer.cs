65af18d48b29c95fce803801dd5229625e5e21e0
Joseph Rapisarda <josephrapisarda0307@gmail.com>
2025-06-29 16:25:31 +0200

    Mp3WavPlayer

---
+++ Mp3/src/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WavPlayerApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.labelVolume = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(195)))), ((int)(((byte)(67)))));
+            this.listBox1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 304);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(114)))), ((int)(((byte)(46)))));
+            this.trackBarVolume.Location = new System.Drawing.Point(278, 12);
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(295, 45);
+            this.trackBarVolume.TabIndex = 1;
+            this.trackBarVolume.Scroll += new System.EventHandler(this.trackBarVolume_Scroll);
+            // 
+            // labelVolume
+            // 
+            this.labelVolume.AutoSize = true;
+            this.labelVolume.BackColor = System.Drawing.Color.Transparent;
+            this.labelVolume.Location = new System.Drawing.Point(278, 60);
+            this.labelVolume.Name = "labelVolume";
+            this.labelVolume.Size = new System.Drawing.Size(35, 13);
+            this.labelVolume.TabIndex = 2;
+            this.labelVolume.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.button1.Location = new System.Drawing.Point(12, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Seleziona cartella audio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(162)))), ((int)(((byte)(216)))));
+            this.ClientSize = new System.Drawing.Size(596, 369);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelVolume);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WAV Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label labelVolume;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Mp3/src/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace WavPlayerApp
+{
+    public partial class Form1 : Form
+    {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.DisplayMember = "Name";
+
+            trackBarVolume.Minimum = 0;
+            trackBarVolume.Maximum = 100;
+            trackBarVolume.Value = 50;
+            labelVolume.Text = $"Volume: {trackBarVolume.Value}%";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            LoadWavFilesFromDirectory(Path.Combine(Application.StartupPath, "Audio"));
+        }
+
+        private void LoadWavFilesFromDirectory(string audioDirectory)
+        {
+            listBox1.Items.Clear();
+
+            if (Directory.Exists(audioDirectory))
+            {
+                var wavFiles = Directory.GetFiles(audioDirectory, "*.wav");
+                var mp3Files = Directory.GetFiles(audioDirectory, "*.mp3");
+                foreach (var file in wavFiles)
+                {
+                    listBox1.Items.Add(new FileInfo(file));
+                }
+                foreach (var file in mp3Files)
+                {
+                    listBox1.Items.Add(new FileInfo(file));
+                }
+            }
+            else
+            {
+                MessageBox.Show("La cartella audio non esiste.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is FileInfo selectedFile)
+            {
+                PlayWavFile(selectedFile.FullName);
+            }
+        }
+
+        private void PlayWavFile(string filePath)
+        {
+            outputDevice?.Stop();
+            outputDevice?.Dispose();
+            audioFile?.Dispose();
+
+            audioFile = new AudioFileReader(filePath);
+            outputDevice = new WaveOutEvent();
+            outputDevice.Init(audioFile);
+
+            audioFile.Volume = trackBarVolume.Value / 100f;
+
+            outputDevice.Play();
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            int volume = trackBarVolume.Value;
+            labelVolume.Text = $"Volume: {volume}%";
+
+            if (audioFile != null)
+            {
+                audioFile.Volume = volume / 100f;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Seleziona la cartella contenente i file WAV";
+                folderDialog.ShowNewFolderButton = false;
+
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderDialog.SelectedPath;
+                    LoadWavFilesFromDirectory(selectedPath);
+                }
+            }
+        }
+    }
+}
